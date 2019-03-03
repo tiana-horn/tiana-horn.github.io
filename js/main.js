efb92c35790b0778c82afa20c25e50b9762e3021
tiana-horn <tiana.horn@gmail.com>
2019-03-03 01:09:20 -0500

    Styling

--- js/main.js
+++ js/main.js
@@ -1,16 +1,44 @@
+// var Menu = {
+//   el: {
+//     menu: $('.menu'),
+//     menuTop: $('.menu-top'),
+//     menuClose: $('.menu-close'),
+//     menuMiddle: $('.menu-middle'),
+//     menuBottom: $('.menu-bottom'),
+//     menuText: $('.menu-text')
+//   },
+  
+//   init: function() {
+//     Menu.bindUIactions();
+//   },
+  
+//   bindUIactions: function() {
+//     Menu.el.menu
+//         .on(
+//           'click',
+//         function(event) {
+//         Menu.activateMenu(event);
+//         event.preventDefault();
+//       }
+//     );
+//   },
+  
+//   activateMenu: function() {
+//     Menu.el.menuTop.toggleClass('menu-top-expand expand');
+//     Menu.el.menuMiddle.toggleClass('menu-middle-expand expand');
+//     Menu.el.menuBottom.toggleClass('menu-bottom-expand expand'); 
+//     Menu.el.menuText.toggleClass('menu-text-expand');
+//     Menu.el.menuClose.toggleClass('menu-close-visible');
+//   }
+// };
+  
+//   //Stop menu item click closing the menu
+//   $(".menu .menu-global").click(function(e) {
+//       e.stopPropagation();
+// });
 
-function get(id) {
-    object = document.getElementById(id)
-    return object
-}
+// Menu.init();
 
-for (var headshot of document.querySelectorAll("tiana-headshot")) {
-    headshot.addEventListener('mouseover', function (event) {
-      headshot.src = headshot.dataset['gif']
-    })
-    headshot.addEventListener('mouseout', function (event) {
-       headshot.src = headshot.dataset['src']
-    })
-  }
 
-function(){e.hide()}}),t("tiana-headshot[data-gif]").hover(function(){var e=t(this);e.attr("src",e.data("gif"))},)
+
+
